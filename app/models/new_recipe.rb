c655e5abdb3100002cf3245a00d86373526e5f00
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-10 11:24:39 +0000

    adding logic for new_recipe model

---
+++ app/models/new_recipe.rb
@@ -0,0 +1,11 @@
+class NewRecipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods
+  has_many :foods, through: :recipe_foods
+
+  validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :cooking_time, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :description, presence: true
+  validates :public, inclusion: { in: [true, false] }
+end
