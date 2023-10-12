f8af7bad67d2b6dae015ac11ff7dbc1e3fef58fe
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-12 18:32:50 +0000

    Fix max line

--- spec/features/new_recipes_index_spec.rb
+++ spec/features/new_recipes_index_spec.rb
@@ -18,7 +18,6 @@ RSpec.feature 'NewRecipe index', type: :feature do
       user: @user
     )
 
-
     visit new_user_session_path
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
