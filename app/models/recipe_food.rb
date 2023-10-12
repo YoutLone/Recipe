7a03cd729ee9af996ccc341bed75ee2b0772b2ce
Yout <youtlone95@gmail.com>
2023-10-12 18:24:05 +0630

    update of recipe food model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,13 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe, class_name: 'NewRecipe'
+  belongs_to :new_recipe
   belongs_to :food
+
+  def food_name
+    food&.name
+  end
+
+  def total_price
+    quantity * food.price
+  end
 end
