4aac072b8242300c93fe04cfa9d878d632c6188c
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-12 14:04:16 +0000

    adding test spec for food

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  it 'is valid with valid attributes' do
+    user = User.create(email: 'test@example.com', password: 'password')
+    food = Food.new(name: 'Test Food', measurement_unit: 'grams', price: 5.99, quantity: 10, user: user)
+    expect(food).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    food = Food.new(measurement_unit: 'grams', price: 5.99, quantity: 10)
+    expect(food).to_not be_valid
+  end
+  it 'is not valid without a measurement_unit' do
+    food = Food.new(name: 'Test food', price: 5.99, quantity: 10)
+    expect(food).to_not be_valid
+  end
+  it 'is not valid without a price' do
+    food = Food.new(name: 'Test food', measurement_unit: 'grams', quantity: 10)
+    expect(food).to_not be_valid
+  end
+  it 'is not valid without a quantity' do
+    food = Food.new(name: 'Test food', measurement_unit: 'grams', price: 5.99)
+    expect(food).to_not be_valid
+  end
+end
