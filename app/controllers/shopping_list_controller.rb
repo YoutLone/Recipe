3b8cacb69d1111ea562ace5ec5fa8da737f69b7b
Yout <youtlone95@gmail.com>
2023-10-13 12:26:09 +0630

    Implement the public recipe design to align given wireframes

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,6 +1,5 @@
 class ShoppingListController < ApplicationController
   def index
-    # @items = RecipeFood.where(recipe_id: params[:id])
     @items = RecipeFood.includes(:food, :new_recipe)
       .select('recipe_foods.*, foods.price * recipe_foods.quantity AS total_price')
       .joins(:food)
